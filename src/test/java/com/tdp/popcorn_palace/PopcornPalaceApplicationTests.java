64eef3be316386a029cf01744eb37716eba2a911
Bar Kobi <122670126+barkobi40@users.noreply.github.com>
2025-03-24 17:43:36 +0200

    last changes

--- src/test/java/com/tdp/popcorn_palace/PopcornPalaceApplicationTests.java
+++ src/test/java/com/tdp/popcorn_palace/PopcornPalaceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.tdp.popcorn_palace;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.tdp.popcorn_palace.controller.MovieController;
+import com.tdp.popcorn_palace.service.ShowtimeService;
 
 @SpringBootTest
 class PopcornPalaceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MovieController movieController;
 
+    @Autowired
+    private ShowtimeService showtimeService;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(movieController, "MovieController should be loaded in the context");
+        assertNotNull(showtimeService, "ShowtimeService should be loaded in the context");
+    }
 }
